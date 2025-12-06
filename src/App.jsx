d6017d0e80bcf0846fdb527035ce7600c9dfb990
MikeQih <2537175447@qq.com>
2025-12-07 01:07:07 +0800

    Update on Merging

--- src/App.jsx
+++ src/App.jsx
@@ -24,23 +24,10 @@ function App() {
           <Route path="/game/select" element={<GameSelection />} />
           <Route path="/game/connect4/tutorial" element={<Tutorial />} />
           <Route path="/game/connect4/play" element={<Connect4Game />} />
-          {/* 后续可以在这里添加更多页面路由 */}
+          <Route path="/game/matchingpairs" element={<MatchingPairs />} />
         </Routes>
       </Router>
     </UIModelProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/game/select" element={<GameSelection />} />
-        <Route path="/game/connect4/tutorial" element={<Tutorial />} />
-        <Route path="/game/matchingpairs" element={<MatchingPairs />} />
-        {/* 后续可以在这里添加更多页面路由 */}
-      </Routes>
-    </Router>
   )
 }
 
